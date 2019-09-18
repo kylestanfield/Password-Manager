15a8925e938fced7891586928dbcb1b51552ad8e
Kyle <stanfield@ucsb.edu>
2019-09-17 17:15:49 -0700

    opa python style

--- manager.py
+++ manager.py
@@ -2,36 +2,40 @@
 import hashlib
 import sqlite3
 
-user_name = raw_input("Enter your user name: ")
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS users (user_name text, pass_hash text)")
-c.execute("SELECT * FROM users where user_name=?", (user_name,))
-if not c.fetchone(): #If the username isn't in the Database
-    resp = ''
-    while resp != 'y' and resp != 'n':
-        resp = raw_input("User name not found." +
-            " Create a new user? (Y/N): ").lower()
-    if resp == 'y':
-        user_pass = raw_input("Enter your password: ")
-        confirm_pass = ''
-        while user_pass and confirm_pass != user_pass:
-            confirm_pass = raw_input("Confirm your password: ")
-            if confirm_pass != user_pass:
-                user_pass = ("Re-enter your password: ")
+if __name__ == '__main__':
+    USER_NAME = input("Enter your user name: ")
+    CONN = sqlite3.connect('users.db')
+    C = CONN.cursor()
+    C.execute("CREATE TABLE IF NOT EXISTS users (user_name text, pass_hash text)")
+    C.execute("SELECT * FROM users where user_name=?", (USER_NAME,))
+    if not C.fetchone(): #If the username isn't in the Database
+        RESP = ''
+        while RESP not in ('y', 'n'):
+            RESP = input("User name not found." +
+                         " Create a new user? (Y/N): ").lower()
 
-        hash = hashlib.sha256(str.encode(user_pass)).hexdigest()
-        c.execute("INSERT INTO users VALUES (?, ?)", (user_name, hash))
-        del user_pass, confirm_pass
-    else:
-        print("Goodbye")
-else: #There was a record with a matching username
-    user_pass = raw_input("Enter your password: ")
-    c.execute("SELECT * FROM users where user_name=?", (user_name,))
-    if c.fetchone()[1] == hashlib.sha256(str.encode(user_pass)).hexdigest():
-        print("Login Success!") #DEBUG
-    else:
-        print("Login fail.")
-    del user_pass
-conn.commit()
-conn.close()
+        if RESP == 'y':
+            USER_PASS = input("Enter your password: ")
+            CONFIRM_PASS = ''
+            while USER_PASS and CONFIRM_PASS != USER_PASS:
+                CONFIRM_PASS = input("Confirm your password: ")
+                if CONFIRM_PASS != USER_PASS:
+                    USER_PASS = ("Re-enter your password: ")
+
+            P_HASH = hashlib.sha256(str.encode(USER_PASS)).hexdigest()
+            C.execute("INSERT INTO users VALUES (?, ?)", (USER_NAME, P_HASH))
+            del USER_PASS, CONFIRM_PASS
+        else:
+            print("Goodbye")
+
+    else: #There was a record with a matching username
+        USER_PASS = input("Enter your password: ")
+        C.execute("SELECT * FROM users where user_name=?", (USER_NAME,))
+        if C.fetchone()[1] == hashlib.sha256(str.encode(USER_PASS)).hexdigest():
+            print("Login Success!") #DEBUG
+        else:
+            print("Login fail.") #dEBUG
+        del USER_PASS
+
+    CONN.commit()
+    CONN.close()
